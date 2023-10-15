44b9a84687dd8938517a225d9ee0d44a4dba5c2d
ze978 <uzzz7899@gmail.com>
2023-10-15 15:29:47 +0900

    구글 트렌드 컴포넌트화

---
+++ src/content/page5.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import Googlegeo from "./googlegeo";
+import Googletime from "./googletime";
+import { googleData } from "./fishData";
+// 수출 페이지
+const Page5 = () => {
+  const [content, setContent] = useState("");
+
+  const handleClickButton = (e) => {
+    const { name } = e.target;
+    setContent(name);
+  };
+
+  // 트렌드 페이지
+  const selectComponent = {
+    Geo: <Googlegeo />,
+    Time: <Googletime />,
+  };
+
+  return (
+    <div className="all mt-4">
+      <h1>ㅤ</h1>
+      {googleData.map((data) => (
+        <button onClick={handleClickButton} name={data.name} key={data.key}>
+          {data.text}
+        </button>
+      ))}
+
+      {/* 선택한 컴포넌트 렌더링 */}
+      {selectComponent[content]}
+    </div>
+  );
+};
+
+export default Page5;
